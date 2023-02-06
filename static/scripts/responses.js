d56c20113eb8d6f7a6ac621be844492fa69db622
lelesmagalhaes <116221684+lelesmagalhaes@users.noreply.github.com>
2023-02-06 18:28:23 -0300

    t4

--- static/scripts/responses.js
+++ static/scripts/responses.js
@@ -13,28 +13,36 @@ var update = {
 function getBotResponse(input, numPergunta) {
     
     if (numPergunta == 0){
+        update["nome_comercial"] = input;
         return "Qual o seu email?";
     } else if (numPergunta == 1) {
+        update["email_comercial"] = input;
         return "Qual CNPJ deseja fazer a consulta? (DIGITE APENAS OS NUMEROS)";
     } else if (numPergunta == 2) {
+
+        update["cnpj"] = input;
         return "Confira se as informações do CNPJ informado estão corretas, digite SIM ou NAO: ";
     } else if (numPergunta == 3) {
-        if (input == 'SIM' || input == 'sim' || input == 's') {
+        if (input.toLowerCase() == 'sim' || input.toLowerCase() == 's') {
             return "O CNPJ informado se encontra no regime de tributação LUCRO REAL, ou LUCRO PRESUMIDO?";
         } else {
             return 'Verifique se o CNPJ está correto, e o digite novamente. (DIGITE APENAS OS NÚMEROS)';
         }
     } else if (numPergunta == 4) {
+        update["regime_tributacao"] = input;
         return "Qual o faturamento anual aproximado?";
     } else if (numPergunta == 5) {
+        update["faturamento_anual"] = input;
         return "Qual a quantidade de funcionários aproximada?";
     } else if (numPergunta == 6) {
+        update["qtd_funcionarios"] = input;
         return "Qual o valor da folha de pagamento (R$ milhões) aproximada?";
     } else if (numPergunta == 7) {
-         update["folha_salarial"] = input;
+        update["folha_salarial"] = input;
+
         var valida = {
             "cnpj": update["cnpj"]
-        };
+        }
 
         var options = {
             method: 'POST',
@@ -45,6 +53,7 @@ function getBotResponse(input, numPergunta) {
             };
 
         fetch('http://localhost:8080/empresas/valida', options).then(resp => console.log(resp)).catch(e => {console.log(e);});
+
         return "Obrigado pelas informações. A LMA AI irá analisar e retorna a você em 72h! Obrigado pela preferência.";
     } 
 
